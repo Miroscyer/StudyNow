a883f473278587908980b8d335a0b08b4eb45137
Miros <253233345@qq.com>
2023-08-12 13:05:09 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,20 @@
+"use strict";
+
+const btnStudy = document.querySelector(".btnStudy");
+const labelStudyTime = document.querySelector(".studyTime");
+const labelTotalTime = document.querySelector(".totalTime");
+let studyArr = [];
+
 const calcRandomStudyTime = function () {
-    const random = Math.random() * 60;
-    console.log(random);
-    return random
-}
+  const random = Math.trunc(Math.random() * 60) + 1;
+  console.log(random);
+  studyArr.push(random);
+
+  labelStudyTime.textContent = `Study tims is ${random}`;
+  const studyHours = studyArr.map((random) => random / 60);
+  //   studyHours.reduce()
+  console.log(studyHours);
+  labelTotalTime.textContent = `Total study time is ${studyHours}`;
+};
+
+btnStudy.addEventListener("click", calcRandomStudyTime);
